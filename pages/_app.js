1b7257ea622a594a3526c4e0c271105de656320c
cti1650 <cti1650@gmail.com>
2021-09-19 16:27:45 +0900

    🐛 Tailwindがうまく反映されない事象に対応

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import 'tailwindcss/tailwind.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className='bg-gray-800 p-4'>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
